8d1b07cbc1b302c668625f681f4bd79c75d87ab6
Sekiraw <90633951+Sekiraw@users.noreply.github.com>
2022-05-02 19:17:38 +0200

    before handling in

--- crosswalk.py
+++ crosswalk.py
@@ -3,15 +3,19 @@ import numpy as np
 
 
 class Beadando:
-    def __init__(self):
-        input = cv2.imread('3.png')
+    def __init__(self, location, result_name):
+        input = cv2.imread(location)
+
+        # setup
+        self.res_name = result_name
+        self.img = None
+        self.thresh = None
+        self.contours = None
 
         # resize the image if its too big
-        self.size(input)
+        self.resize(input)
 
-        self.contours_combined = []
-
-    def size(self, input):
+    def resize(self, input):
         # print("input shapes \n" + str(input.shape[0]) + " height \n" + str(input.shape[1]) + " width")
 
         if input.shape[0] > 1000 or input.shape[1] > 1000:
@@ -36,33 +40,35 @@ class Beadando:
         return morph
 
     def filter_by_area(self, morph):
-        cntrs = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cntrs = cntrs[0] if len(cntrs) == 2 else cntrs[1]
         self.contours = self.img.copy()
-        good_contours = []
-        for c in cntrs:
+
+        contours = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) == 2:
+            contours = contours[0]
+        else:
+            contours = contours[1]
+
+        for c in contours:
             area = cv2.contourArea(c)
             if area > 800:
                 cv2.drawContours(self.contours, [c], -1, (0, 0, 255), thickness=cv2.FILLED)
-                good_contours.append(c)
-
-        # combine good contours
-        self.contours_combined = np.vstack(good_contours)
-
-        return self.contours_combined
 
     def s_and_s(self):
-        cv2.imwrite("eredmeny.jpg", self.contours)
-
-        cv2.imshow("Eredmeny", self.contours)
+        cv2.imwrite("result-of-" + self.res_name + ".jpg", self.contours)
+        cv2.imshow("Eredmeny-" + self.res_name, self.contours)
 
     def run(self):
         self.filter_by_area(self.filter())
         self.s_and_s()
 
-        cv2.waitKey(0)
-
 
 if __name__ == "__main__":
-    bead = Beadando()
-    bead.run()
+    kep1 = Beadando('1.jpeg', "kep1")
+    kep2 = Beadando('2.jpg', "kep2")
+    kep3 = Beadando('3.png', "kep3")
+    kep1.run()
+    kep2.run()
+    kep3.run()
+
+    cv2.waitKey(0)
